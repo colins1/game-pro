5e1e1f6df8f13e0b6bcbf33f9c1d0d6f13285200
colins1 <88490207+colins1@users.noreply.github.com>
2022-08-23 17:56:52 +0300

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,19 @@
 let item = document.querySelectorAll('div.b-item')
 let i = 0
 let it = 0
+let run = 6
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-let interval = setInterval(function() {
+let intervals = setInterval(function() {
     if (it == 0) {
         let run = getRandomInt(item.length)
         i = run
         let elem = document.createElement("img");
         elem.setAttribute("src", "goblin.png");
+        elem.classList.add("img");
         random(item[run], elem)
     } else {
         let run = getRandomInt(item.length)
@@ -23,29 +26,59 @@ let interval = setInterval(function() {
             i = run
             let elem = document.createElement("img");
             elem.setAttribute("src", "goblin.png");
+            elem.classList.add("img");
             random(item[run], elem)
         } else {
             i = run
             let elem = document.createElement("img");
             elem.setAttribute("src", "goblin.png");
+            elem.classList.add("img");
             random(item[run], elem)
         }
     }
     ++it
+    run--
+if (run != 0) {
     
-}, 1000);
+} else {
+	document.querySelector(".check-result").textContent = "LOST"
+	clearInterval(intervals);
+}
+    
+}, 1500);
 
   
 function random(randoms, elem) {
     let interval = setInterval(function() {
         randoms.appendChild(elem)
-    }, 1000);
+    }, 500);
     setTimeout(function() {
         clearInterval(interval);
-    }, 1000); 
+    }, 500); 
     let rem = document.querySelector('img')
     if (rem != null) {
         rem.remove()
     }
 
 }
+
+
+const eventClick = document.querySelectorAll(".b-item").forEach(box =>
+    box.addEventListener("click", function () {
+            if (box.children[0] != undefined) {
+                if (document.querySelector(".check-result").textContent == "4") {
+                    document.querySelector(".check-result").textContent = "WIN"
+                    clearInterval(intervals);
+                    document.querySelectorAll(".img").forEach(box =>
+                        box.remove()
+                    )
+                } else {
+                    document.querySelector(".check-result").textContent++
+                    let rem = document.querySelector('img')
+                    rem.remove()
+                }
+                run++
+            }
+
+    })
+)
